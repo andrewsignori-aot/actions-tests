d0440f2bf8935319f5e347524318ba4f67709d63
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2023-05-31 15:36:49 -0700

    #123 - Feature done (#10)
    Change dobe!

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 // v1.0.0 - change 1
 // v1.0.0 - change 2
 // v1.0.0 - change 3
+// v1.0.0 - changed on branch
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
