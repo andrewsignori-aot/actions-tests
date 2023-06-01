3823f78ed88c17e2da6607e19d3c6586b60001f3
Andrew Boni Signori <andrew.signori@aot-technologies.com>
2023-06-01 12:42:21 -0700

    #2222 - Change 2

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // #1111 - Change 1
+// #2222 - Change 2
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
