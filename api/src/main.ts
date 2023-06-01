545930e07bb83d39efd47608985a6a800c515609
Andrew Boni Signori <61259237+andrewsignori-aot@users.noreply.github.com>
2023-06-01 13:06:16 -0700

    #444 Fix for release (#15)
    Fix for release.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// #1111 - Change 1
-// #2222 - Change 2
+// #1111 - Change one
+// #2222 - Change two
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
